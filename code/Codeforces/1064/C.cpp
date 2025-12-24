12b315e30da80eccfb159fece154c5daf16f7028
Alejandro Guajardo <alexguajardoc0902@gmail.com>
2025-12-23 20:58:36 -0600

    daily 23/12/25

---
+++ code/Codeforces/1064/C.cpp
@@ -0,0 +1,58 @@
+// Problem: C. Oh Those Palindromes
+// Contest: Codeforces - Codeforces Round 516 (Div. 2, by Moscow Team Olympiad)
+// URL: https://codeforces.com/contest/1064/problem/C
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ln "\n"
+#define fast_cin() \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+#define iofiles() \
+    freopen("input.in", "r", stdin); \
+    freopen("output.out", "w", stdout)
+#define dbg(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char *name, Arg1 &&arg1) { cout << name << ": " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f(const char *names, Arg1 &&arg1, Args &&... args) {
+    const char *comma = strchr(names + 1, ',');
+    cout.write(names, comma - names) << ": " << arg1 << " |";
+    __f(comma + 1, args...);
+}
+
+#define ll long long
+#define int ll
+#define ld long double
+#define pb push_back
+#define INF 2e18
+#define PI 3.14159265358979323846
+#define MOD 1000000007
+
+double eps = 1e-9;
+
+void solve() {
+    int n;
+    string s;
+    cin >> n >> s;
+    sort(s.begin(), s.end());
+    cout << s << ln;
+}
+
+signed main() {
+    fast_cin();
+    
+    int T = 1;
+    // cin >> T;
+    for (int i = 1; i <= T; i++) {
+        solve(  );
+    }
+
+    return 0;
+}
+// g++ A.cpp && ./a.out <input.in>output.out
