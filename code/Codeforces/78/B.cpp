e5bf5e4313a2d37f95c11284476dfc7104b0356c
Alejandro Guajardo <alexguajardoc0902@gmail.com>
2026-02-20 20:15:23 -0600

    daily  16/2/26

---
+++ code/Codeforces/78/B.cpp
@@ -0,0 +1,64 @@
+// Problem: B. Easter Eggs
+// Contest: Codeforces - Codeforces Beta Round 70 (Div. 2)
+// URL: https://codeforces.com/contest/78/problem/B
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ln "\n"
+#define fast_cin() \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+#define iofiles() \
+    freopen("input.in", "r", stdin); \
+    freopen("output.out", "w", stdout)
+#define dbg(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char *name, Arg1 &&arg1) { cout << name << ": " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f(const char *names, Arg1 &&arg1, Args &&... args) {
+    const char *comma = strchr(names + 1, ',');
+    cout.write(names, comma - names) << ": " << arg1 << " |";
+    __f(comma + 1, args...);
+}
+
+#define ll long long
+#define int ll
+#define ld long double
+#define pb push_back
+
+const ll INF = LLONG_MAX / 4;
+const ld PI = acos(-1);
+const int MOD = 1000000007;
+const double eps = 1e-9;
+
+void solve() {
+    string x = "ROYGBIV";
+    string x2 = "GBIV";
+    int y = x2.length();
+    int n;
+    cin >> n;
+    
+    cout << x;
+    
+    for (int i = 7; i < n; i++) {
+    	cout << x2[(i-7)%y];
+    }
+}
+
+signed main() {
+    fast_cin();
+    
+    int T = 1;
+    // cin >> T;
+    for (int i = 1; i <= T; i++) {
+        solve(  );
+    }
+
+    return 0;
+}
+// g++ A.cpp && ./a.out <input.in>output.out
