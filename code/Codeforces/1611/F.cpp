953369b1d0bc58d6f65864ef49b48072dbf452a7
Alejandro Guajardo <alexguajardoc0902@gmail.com>
2026-03-05 08:04:11 -0600

    daily 3/3/26

---
+++ code/Codeforces/1611/F.cpp
@@ -0,0 +1,147 @@
+// Problem: F. ATM and Students
+// Contest: Codeforces - Codeforces Round 756 (Div. 3)
+// URL: https://codeforces.com/contest/1611/problem/F
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ln "\n"
+#define fast_cin() \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+#define iofiles() \
+    freopen("input.in", "r", stdin); \
+    freopen("output.out", "w", stdout)
+#define dbg(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char *name, Arg1 &&arg1) { cout << name << ": " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f(const char *names, Arg1 &&arg1, Args &&... args) {
+    const char *comma = strchr(names + 1, ',');
+    cout.write(names, comma - names) << ": " << arg1 << " |";
+    __f(comma + 1, args...);
+}
+
+#define ll long long
+#define int ll
+#define ld long double
+#define pb push_back
+
+const ll INF = LLONG_MAX / 4;
+const ld PI = acos(-1);
+const int MOD = 1000000007;
+const double eps = 1e-9;
+
+struct Segtree {
+	vector<int> st;
+	int TREE_SIZE = 1;
+	
+	Segtree(int n) {
+		while (TREE_SIZE <= n) TREE_SIZE <<= 1;
+		st.assign(2 * TREE_SIZE, INF);
+	}
+	
+	void update(int i, int x) {
+		i += TREE_SIZE;
+		st[i] = x;
+		
+		i >>= 1;
+		while(i) {
+			st[i] = min(st[2*i], st[2 * i + 1]);
+			
+			i >>= 1;
+		}
+	}
+	
+	int query(int l, int r) {
+		l += TREE_SIZE;
+		r += TREE_SIZE;
+		
+		int res = INF;
+		while (l <= r) {
+			if (l&1) res = min(res, st[l++]);
+			if (!(r&1)) res = min(res, st[r--]);
+			
+			l >>= 1;
+			r >>= 1;
+		}
+		
+		return res;
+	}
+};
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (auto& i : a) cin >> i;
+    
+    vector<int> d;
+    Segtree st(n);
+    int prev = 0;
+    for (int i = 0; i < n; i++) {
+    	d.pb(prev + a[i]);
+    	
+    	prev = d.back();
+    	// dbg(d.back());
+    	
+    	st.update(i, d[i]);
+    }
+    
+    // BS over segtree, min needs to be >=
+    // d[i] - k
+    
+    int L = -1, R = -1;
+    prev = 0;
+    for (int i = 0; i < n; i++) {
+    	int l = i, r = n - 1;
+    	if (a[i] < -k) {
+    		prev = d[i];
+    		continue;
+    	}
+    	// dbg(i);
+    	while (l <= r) {
+    		int m = (l + r) / 2;
+    		
+    		int v = st.query(i, m) - prev;
+    		int x = - k;
+    		// dbg(v, x, m);
+    		
+    		if (v >= x) {
+    			if (L == -1 or (R - L) < m - i) {
+    				L = i, R = m;
+    			}
+    			l = m + 1;
+    		}
+    		else {
+    			r = m - 1;
+    		}
+    	}
+    	
+    	prev = d[i];
+    }
+    
+    if (L == -1) {
+    	cout << -1 << ln;
+    }
+    else {
+    	cout << L + 1 << " " << R + 1 << ln;
+    }
+}
+
+signed main() {
+    fast_cin();
+    
+    int T = 1;
+    cin >> T;
+    for (int i = 1; i <= T; i++) {
+        solve(  );
+    }
+
+    return 0;
+}
+// g++ A.cpp && ./a.out <input.in>output.out
