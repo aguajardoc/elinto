00431fe5027529f62056552ba4b136cc10a5cb81
Alejandro Guajardo <109005317+aguajardoc@users.noreply.github.com>
2026-04-09 10:58:22 -0600

    Update random.cpp

--- templates/random.cpp
+++ templates/random.cpp
@@ -1,2 +1,6 @@
 // Usage: rng();
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+// Get random integer in range
+const int B = uniform_int_distribution<int>(lb, ub)(rng);
+// Shuffle an array
+shuffle(a.begin(), a.end(), rng);
