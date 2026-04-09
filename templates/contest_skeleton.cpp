8e71c33ef87b36620b962412bbf7b5753e13071b
Alejandro Guajardo <alexguajardoc0902@gmail.com>
2026-04-09 11:44:56 -0600

    Create contest_skeleton.cpp

---
+++ templates/contest_skeleton.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ln "\n"
+#define fast_cin() \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+#define ll long long
+#define lli long long
+#define ld long double
+#define pb push_back
+
+const ll INF = LLONG_MAX / 4;
+const ld PI = acos(-1);
+const int MOD = 1000000007;
+const double eps = 1e-9;
+
+void solve() {
+    
+}
+
+signed main() {
+    fast_cin();
+    
+    int T = 1;
+    cin >> T;
+    for (int i = 1; i <= T; i++) {
+        solve(  );
+    }
+
+    return 0;
+}
+// g++ A.cpp && ./a.out <input.in>output.out
